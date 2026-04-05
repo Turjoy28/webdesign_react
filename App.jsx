000d17f34e2e68ee9b4f62465cae3633a6ffe07e
Saif_Sauwar_Turjoy <saifbus28@gmail.com>
2026-04-06 05:52:05 +0600

    Add simple App.jsx component

---
+++ App.jsx
@@ -0,0 +1,12 @@
+import React from 'react';
+
+const App = () => {
+    return (
+        <div>
+            <h1>Hello, welcome to my app!</h1>
+            <p>This is a simple App component.</p>
+        </div>
+    );
+};
+
+export default App;
